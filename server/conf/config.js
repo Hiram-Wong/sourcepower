2a95a9361273a2c3bc1c66a83e9df883c607dd9d
Hiram <>
2024-09-12 19:09:27 +0800

    modify env the field

--- server/conf/config.js
+++ server/conf/config.js
@@ -30,10 +30,10 @@ const config = {
   knex: {
     client: "pg",
     connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
     },
   },
 };
