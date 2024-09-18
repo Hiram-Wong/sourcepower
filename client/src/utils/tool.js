2b81112c1bfd4f6e40fcba8052428f2de3f7b779
Hiram <>
2024-09-18 22:15:37 +0800

    fix avatar method

--- client/src/utils/tool.js
+++ client/src/utils/tool.js
@@ -1,3 +1,6 @@
+import imgAvatar from '@/assets/avatar.png';
+import imgAvatarDefault from '@/assets/avatar.png';
+
 const formatDate = (str) => {
   const date = new Date(str);
   const year = date.getFullYear();
@@ -8,17 +11,21 @@ const formatDate = (str) => {
 };
 
 const formatAvatar = (str) => {
-  const email = str.toLowerCase();
-  if (email.includes("@qq.com")) {
-    const prefix = email.replace("@qq.com", "");
-    if (prefix.match(/^\d+$/)) {
-      return `https://q1.qlogo.cn/g?b=qq&nk=${prefix}&s=640`;
-      // return `https://q2.qlogo.cn/headimg_dl?dst_uin=${prefix}&spec=100`;
+  if (str) {
+    const email = str.toLowerCase();
+    if (email.includes("@qq.com")) {
+      const prefix = email.replace("@qq.com", "");
+      if (prefix.match(/^\d+$/)) {
+        return `https://q1.qlogo.cn/g?b=qq&nk=${prefix}&s=640`;
+        // return `https://q2.qlogo.cn/headimg_dl?dst_uin=${prefix}&spec=100`;
+      } else {
+        return imgAvatar;
+      }
     } else {
-      return '@/assets/avatar.png';
+      return imgAvatar;
     }
   } else {
-    return '@/assets/avatar.png';
+    return imgAvatarDefault;
   }
 };
 
